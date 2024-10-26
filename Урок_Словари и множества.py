9931ba0455d5fc17b823ca1f3c92be5bee3bb732
Катя <katja48tab42794@gmail.com>
2024-10-26 15:21:47 +0300

    while и for

--- Урок_Словари и множества.py
+++ Урок_Словари и множества.py
@@ -19,3 +19,23 @@ print(phone_book)
 print(phone_book.get('Kei'))     #выводит значение ключа
 print(phone_book.get('Denis'))   # None если нет такого ключа
 print(phone_book.get('Denis',"такого ключа нет")) # или запись
+
+'''pop''' # извлекает ключ-значение
+print(phone_book)
+'''phone_book.pop('Катя')'''  # удаляет ключ
+a = phone_book.pop('Катя')    # ,но можно сохранить его значение в переменной
+print("'Катя'удалили, значение сохранили в переменную a =",a)
+print(phone_book)
+
+# со списками по индексу
+list_=[1,2,3]
+list_.pop(0)
+print(list_)
+list_2=[4,5,6,7,8]
+print(list_2)
+b=list_2.pop(1)
+print(list_2)
+print(b)
+print(list_2.pop(1))
+print(list_2)
+
