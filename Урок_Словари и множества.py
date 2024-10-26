38b4e59b70e412e9d28a54641a4e89c30fc3acd2
Катя <katja48tab42794@gmail.com>
2024-10-26 14:29:45 +0300

    while и for

--- Урок_Словари и множества.py
+++ Урок_Словари и множества.py
@@ -1,2 +1,21 @@
-phone_book={"Катя": 89513035859,"Оля":892063764}
+phone_book = {"Катя": 10000, "Оля": 222222}
 print(phone_book)
+phone_book['Катя'] = 33333  # изменить значение ключа
+print(phone_book)
+phone_book['Кира'] = 44444 # новые ключ-значение в конец словаря
+print(phone_book)
+
+'''del'''
+del phone_book['Оля']  # удаление ключ-значение  словаря
+print(phone_book)
+
+'''update'''
+phone_book.update({'Kei':2121,'Alex':3232}) # update добавляет и обновляет несколько
+                                            # пар ключ-значение
+# ??? этот метод принимает другой словарь,который указываем в скобках Scripts ???
+print(phone_book)
+
+'''get'''
+print(phone_book.get('Kei'))     #выводит значение ключа
+print(phone_book.get('Denis'))   # None если нет такого ключа
+print(phone_book.get('Denis',"такого ключа нет")) # или запись
