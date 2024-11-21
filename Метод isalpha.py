be4565c3abca733975ba91df8808287960b7e848
Катя <katja48tab42794@gmail.com>
2024-11-21 22:48:41 +0300

    21/11 Методы rfind и find

---
+++ Метод isalpha.py
@@ -0,0 +1,11 @@
+'''Метод isalpha() возвращает True, если все символы в строке являются алфавитными.
+Если нет, возвращается False.
+Синтаксис: string.isalpha()
+Параметры
+Метод isalpha() в Python не принимает никаких параметров.
+Возвращаемое значение Модуль возвращает:
+Истинно, если все символы в строке являются алфавитными (могут быть как строчными, так и прописными).
+Неверно, если хотя бы один символ не является алфавитом.
+Пример 1: Как работает? name = "Monica" print(name.isalpha()) # contains whitespace name = "Monica Geller" print(name.isalpha()) # contains number name = "Mo3nicaGell22er" print(name.isalpha())
+Источник: https://pythonstart.ru/string/isalpha-python
+  '''
