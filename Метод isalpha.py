2c1943aafb40a73dd4b21dd691df2e0841790fea
Катя <katja48tab42794@gmail.com>
2024-11-23 14:30:38 +0300

    21/11 Метод isalpha() возвращает True, если все символы в строке являются алфавитными. Если нет, возвращается False.

--- Метод isalpha.py
+++ Метод isalpha.py
@@ -6,5 +6,40 @@
 Возвращаемое значение Модуль возвращает:
 Истинно, если все символы в строке являются алфавитными (могут быть как строчными, так и прописными).
 Неверно, если хотя бы один символ не является алфавитом.
-Пример 1: Как работает? name = "Monica" print(name.isalpha()) # contains whitespace name = "Monica Geller" print(name.isalpha()) # contains number name = "Mo3nicaGell22er" print(name.isalpha())
   '''
+print(' Пример 1: Как работает? Метод isalpha() возвращает True, если все символы в строке'
+      ' являются АЛФАВИТНЫМИ. Если нет, возвращается False.')
+
+name = "Monica"
+print('"Monica"',name.isalpha())
+
+# contains whitespace
+name = "Monica Geller"
+print('"Monica Geller"',name.isalpha())
+
+# contains number
+name = "Mo3nicaGell22er"
+print('"Mo3nicaGell22er"',name.isalpha())
+print("______________")
+print("ПРИМЕР")
+name = "098Mon916ler"
+
+if name.isalpha() == True:
+    print(name, '- "Все символы являются алфавитами./All characters are alphabets"')
+else:
+    print(name, '- "Не все символы являются алфавитами./All characters are not alphabets." ')
+
+name = "Monller"
+
+if name.isalpha() == True:
+    print(name, '- "Все символы являются алфавитами./All characters are alphabets"')
+else:
+    print(name, '- "Не все символы являются алфавитами./All characters are not alphabets." ')
+
+name = "Monica Geller"
+
+if name.isalpha() == True:
+    print(name, '- "Все символы являются алфавитами./All characters are alphabets"')
+else:
+    print(name, '- "Не все символы являются алфавитами./All characters are not alphabets." ')
+#
